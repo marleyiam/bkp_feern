987539d6dd8070ede174ae588bf096fbe9c5f929
Rui Anderson <anderson.rui@gmail.com>
2012-10-19 14:11:35 -0300

    Redis production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.development?
   $redis = Redis.new
 else
-  #uri = URI.parse(ENV["REDISTOGO_URL"])
-  #$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
