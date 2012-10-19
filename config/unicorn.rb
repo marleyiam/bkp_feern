23b210f619dad9d385a6dda382bed32f54e9e44b
Rui Anderson <anderson.rui@gmail.com>
2012-10-19 14:18:50 -0300

    Unicorn optimizing

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,15 @@
 worker_processes 4
 timeout 4
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(Mongoid)
+    Mongoid.default_session.disconnect
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(Mongoid)
+    Mongoid.default_session.cluster.refresh
+  end
+end
