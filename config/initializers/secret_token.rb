0047dadfe8a294c47ce41bf4cca1a967bd14825f
Rui Anderson <anderson.rui@gmail.com>
2012-10-19 10:49:45 -0300

    Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feern::Application.config.secret_token = 'ed63646759a1f3df6ed822e8fb0ebb7de5707ebf1a604af977340a0ed4b8920b018510d9e76c66ef22817a0c3f04cc21320d2e69b085bc357f61441d5d2dc8e0'
